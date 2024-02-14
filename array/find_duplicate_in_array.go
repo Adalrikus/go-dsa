e93cba3e6d0847d4d9d389ce2a7f1f6bf5c9aa38
Adilkhan Zarubay <adilkhan.zarubay@ericsson.com>
2024-02-14 22:59:05 +0100

    Find duplicate using hashset

--- array/find_duplicate_in_array.go
+++ array/find_duplicate_in_array.go
@@ -1,13 +1,24 @@
 package array
 
 // FindDuplicate solves the problem in O(n) time and O(1) space.
+type set map[int]struct{}
+
+func (s set) add(number int) {
+	s[number] = struct{}{}
+}
+
+func (s set) has(number int) bool {
+	_, ok := s[number]
+	return ok
+}
+
 func FindDuplicate(list []int) int {
-	for _, item := range list {
-		itemIndex := item - 1
-		if list[itemIndex] < 0 {
-			return list[itemIndex] * -1
+	numSet := set{}
+	for _, v := range list {
+		if numSet.has(v) {
+			return v
 		}
-		list[itemIndex] *= -1
+		numSet.add(v)
 	}
 	return -1
 }
