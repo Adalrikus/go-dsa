c15285c164362f142838304625993d6b25fce1c2
Adilkhan Zarubay <adilkhan.zarubay@ericsson.com>
2024-02-15 00:18:11 +0100

    Product of all other elements

--- array/product_of_all_other_elements.go
+++ array/product_of_all_other_elements.go
@@ -2,34 +2,20 @@ package array
 
 // ProductOfAllOtherElements solves the problem in O(n) time and O(1) space.
 func ProductOfAllOtherElements(list []int) []int {
-	if len(list) == 0 {
-		return list
+	n := len(list)
+	result := make([]int, n)
+
+	prefix := 1
+	for i := 0; i < n; i++ {
+		result[i] = prefix
+		prefix *= list[i]
 	}
 
-	right := make([]int, len(list))
-
-	product := 1
-	for i := len(list) - 1; i >= 0; i-- {
-		product *= list[i]
-		right[i] = product
+	suffix := 1
+	for i := n - 1; i > -1; i-- {
+		result[i] *= suffix
+		suffix *= list[i]
 	}
 
-	product = list[0]
-	for i := 0; i < len(list); i++ {
-		if i == 0 {
-			list[i] = right[1]
-			continue
-		}
-		value := list[i]
-
-		if i != len(list)-1 {
-			list[i] = right[i+1] * product
-		} else {
-			list[i] = product
-		}
-
-		product *= value
-	}
-
-	return list
+	return result
 }
